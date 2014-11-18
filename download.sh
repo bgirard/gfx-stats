8273e5e847cd01278aa9d3541ebbfd177e6d3669
Benoit Girard <b56girard@gmail.com>
2014-11-17 19:19:58 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,4 @@
 CRASH_URL="https://crash-analysis.mozilla.com/crash_analysis/"
-DOWNLOAD_JOBS=16
 
 set -e
 
@@ -17,7 +16,7 @@ function max_bg_procs {
     fi
     local max_number=$((0 + ${1:-0}))
     while true; do
-            local current_number=$(jobs -p | wc -l)
+            local current_number=$(jobs | wc -l)
             if [[ $current_number -lt $max_number ]]; then
                     break
             fi
@@ -28,18 +27,18 @@ function max_bg_procs {
 INDEX_HTML=`curl -s $CRASH_URL`
 
 while read -r line; do
-  if [[ "${line:0:82}" == "<tr><td valign=\"top\"><img src=\"/icons/folder.gif\" alt=\"[DIR]\"></td><td><a href=\"20" ]] ;
+  if [[ "${line:0:84}" == "<tr><td valign=\"top\"><img src=\"/icons/folder.gif\" alt=\"[DIR]\"></td><td><a href=\"2014" ]] ;
   then
     CRASH_DATE=${line:80:8}
     CRASH_FILE=$CRASH_DATE-pub-crashdata.csv.gz
     ls $CRASH_FILE 2>&1 > /dev/null 2> /dev/null || {
       # Download the file
       echo "Fetching $CRASH_FILE"
-      wget --no-check-certificate "$CRASH_URL$CRASH_DATE/$CRASH_FILE" 2> /dev/null > /dev/null &
-      max_bg_procs $DOWNLOAD_JOBS
+      wget --no-check-certificate "$CRASH_URL$CRASH_DATE/$CRASH_FILE" 2> /dev/null > /dev/null || {
+        echo "Skipping $CRASH_FILE"
+      }
     }
   fi
 done <<< "$INDEX_HTML"
 
-max_bg_procs 0
-
+echo "Crash file updated"
